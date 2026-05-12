790ab3df93aa60a4488ffbb5727cfeb6f6c1f5bd
codenavie <gylesperez498@gmail.com>
2026-05-12 15:17:06 +0800

    live

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,7 @@
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/Offline-Knowledge-Base-Search-Engine/",
   plugins: [vue()],
-  server: {
-    port: 5173
-  }
+  server: { port: 5173 }
 });
